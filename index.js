12a40db062e276416dd89ee63435f7f4922ab169
Adam Bray <adam.bray@yale.edu>
2015-12-01 08:59:14 -0500

    cleaned up index.js

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-// express dependency for our application
-var express = require('express');
-// loads mongoose dependency
-var mongoose = require('mongoose');
-// loads dependency for middleware for paramters
-var bodyParser = require('body-parser');
-// loads dependency that allows put and delete where not supported in html
-var methodOverride = require('method-override');
-// loads module containing all authors contrller actions. not defined yet...
-var authorsController = require("./controllers/authorsController");
-// connect mongoose interfaces to reminders mongo db
-mongoose.connect('mongodb://localhost/reminders');
+var express = require('express'); // express dependency for our application
 var app = express();
-// sets view engine to handlebars
-app.set("view engine", "hbs");
-// allows for parameters in JSON and html
-app.use(bodyParser.json());
+
+app.set("view engine", "hbs"); // sets view engine to handlebars
+app.use(express.static(__dirname + '/public')); // connects assets like stylesheets
+
+var mongoose = require('mongoose'); // loads mongoose dependency
+mongoose.connect('mongodb://localhost/reminders'); // connect mongoose interfaces to reminders mongo db
+
+var bodyParser = require('body-parser'); // loads dependency for middleware for paramters
+app.use(bodyParser.json()); // allows for parameters in JSON and html
 app.use(bodyParser.urlencoded({extended:true}));
-// allows for put/delete request in html form
-app.use(methodOverride('_method'));
-// connects assets like stylesheets
-app.use(express.static(__dirname + '/public'));
+
+var methodOverride = require('method-override'); // loads dependency that allows put and delete where not supported in html
+app.use(methodOverride('_method')); // allows for put/delete request in html form
+
+var authorsController = require("./controllers/authorsController"); // loads module containing all authors contrller actions. not defined yet...
 
 // app server located on port 4000
 app.listen(4000, function(){
   console.log("app listening on port 4000");
 });
 
-// first route we'll define together ...
+// routes
 app.get("/authors", authorsController.index);
 app.get("/authors/:id", authorsController.show);
