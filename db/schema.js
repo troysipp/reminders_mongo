267a908faaae06ab4c35da6d671a867cf1bc6426
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-04-06 09:55:11 -0400

    Requires Mongoose and Sets Up DB Connection

---
+++ db/schema.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/reminders');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+  console.log(err);
+});
+
+db.once('open', function() {
+  console.log("Connected to MongoDB!");
+
+});
